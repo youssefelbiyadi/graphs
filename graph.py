e50868a8f7d26ac23f41b3a3589b7356ef2916db
Youssef EL-BIYADI <youssef.el-biyadi@MacBook-Pro-de-Youssef.local>
2023-03-04 18:27:59 +0100

    fmt

--- graph.py
+++ graph.py
@@ -16,7 +16,11 @@ class Graph:
         for vertex, neighbours in self.adjacencies_list.items():
             print(f"{vertex}: {neighbours}")
 
-    def _are_neighbours(self, vertex: str, other_vertex: str) -> bool:
+    def _are_neighbours(
+        self,
+        vertex: str,
+        other_vertex: str,
+    ) -> bool:
         if vertex not in self.adjacencies_list:
             raise GraphError(f"Vertex {vertex} does not exist")
         if other_vertex not in self.adjacencies_list:
@@ -32,15 +36,26 @@ class Graph:
             return True
         return False
 
-    def add_edge(self, vertex: str, other_vertex: str) -> bool:
+    def add_edge(
+        self,
+        vertex: str,
+        other_vertex: str,
+    ) -> bool:
         if vertex in self.adjacencies_list and other_vertex in self.adjacencies_list:
-            if not self._are_neighbours(vertex=vertex, other_vertex=other_vertex):
+            if not self._are_neighbours(
+                vertex=vertex,
+                other_vertex=other_vertex,
+            ):
                 self.adjacencies_list[vertex].append(other_vertex)
                 self.adjacencies_list[other_vertex].append(vertex)
                 return True
         return False
 
-    def remove_vertex_neighbour(self, vertex: str, neighbour: str) -> bool:
+    def remove_vertex_neighbour(
+        self,
+        vertex: str,
+        neighbour: str,
+    ) -> bool:
         if vertex not in self.adjacencies_list:
             return False
 
@@ -49,10 +64,20 @@ class Graph:
             return True
         return False
 
-    def remove_edge(self, vertex: str, other_vertex: str) -> bool:
+    def remove_edge(
+        self,
+        vertex: str,
+        other_vertex: str,
+    ) -> bool:
         if vertex in self.adjacencies_list and other_vertex in self.adjacencies_list:
-            self.remove_vertex_neighbour(vertex=vertex, neighbour=other_vertex)
-            self.remove_vertex_neighbour(vertex=other_vertex, neighbour=vertex)
+            self.remove_vertex_neighbour(
+                vertex=vertex,
+                neighbour=other_vertex,
+            )
+            self.remove_vertex_neighbour(
+                vertex=other_vertex,
+                neighbour=vertex,
+            )
             return True
         return False
 
@@ -60,7 +85,10 @@ class Graph:
         if vertex in self.adjacencies_list:
             vertex_neighbours = self.adjacencies_list[vertex].copy()
             for neighbour in vertex_neighbours:
-                self.remove_edge(vertex=vertex, other_vertex=neighbour)
+                self.remove_edge(
+                    vertex=vertex,
+                    other_vertex=neighbour,
+                )
             del self.adjacencies_list[vertex]
             return True
         return False
